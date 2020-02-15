0a6c152796c19a55468b649a1a98e98b95217d91
Gauthier Cler <gauthier.cler@epitech.eu>
2020-02-15 21:32:43 +0100

    clean code

--- kmean.py
+++ kmean.py
@@ -56,47 +56,7 @@ def cluster(texts, ref, clusters_nb):
         break
 
     final = [x for x in texts if x in sentences]
-
-    # order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-    # terms = vec.get_feature_names()
-    # labels = []
-    # for i in range(clusters_nb):
-    #     top_ten_words = [terms[ind] for ind in order_centroids[i, :5]]
-    #     labels.append(' '.join(top_ten_words))
-
     return ' '.join(final)
-
-    # results = pd.DataFrame({
-    #     'category': km.labels_,
-    #     'similarity': cosine_similarities,
-    #     'text': texts
-    # })
-    #
-    # results.sort_values(by=['similarity'], inplace=True, ascending=False)
-    # print(results)
-    #
-    # clusters_indices = [0] * number_of_clusters
-    # for index, c in enumerate(km.labels_):
-    #     clusters_indices[c] += cosine_similarities[index]
-    #
-    # for sim in clusters_indices:
-    #     print(sim)
-    #
-    # patches, text, _ = plt.pie(clusters_indices, autopct='%1.1f%%')
-    # plt.legend(patches, labels, bbox_to_anchor=(0, 0), loc="upper left")
-    # plt.show()
-    # results.to_csv("out.csv")
-    #
-    # print(sorted(Counter(km.labels_).items()))
-    #
-    # print(matrix[km.labels_ == 0][0])
-    # # print(len(km.cluster_centers_[0]))
-    #
-    # # plt.scatter()
-    # # plt.scatter(clusters[:, 0], clusters[:, 1], s=20, c=km.labels_.tolist())
-    # # plt.show()
-    #
-    # return
 
 
 def kmean(text, ref):
